00a82d6e7c2cd5505e6759e5c0fc60b7e3862592
Felix Vemmer <felix.vemmer@gmail.com>
2021-11-19 09:15:59 +0100

    feat: configures network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,21 @@
-require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-waffle');
+require('dotenv').config();
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+const privateKey = process.env.PRIVATE_KEY;
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: '0.8.4',
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: {
+      url: process.env.POLYGON_MUMBAI_KEY_DEV,
+      accounts: [privateKey],
+    },
+    mainnet: {
+      url: process.env.POLYGON_MUMBAI_KEY_PROD,
+      accounts: [privateKey],
+    },
+  },
 };
