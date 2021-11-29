45ba3f9e4236800e011e5a4e7b7d51bf244fa562
Felix Vemmer <felix.vemmer@gmail.com>
2021-11-29 23:07:51 +0100

    fix: fixes action button links

--- components/Hero.js
+++ components/Hero.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 function Hero() {
   return (
@@ -23,20 +24,24 @@ function Hero() {
         </p>
         <div className="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
           <div className="rounded-md shadow">
+            <Link href="/portfolio">
             <a
-              href="#"
+
               className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 md:py-4 md:text-lg md:px-10"
             >
               Portfolio Projects
             </a>
+            </Link>
           </div>
           <div className="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
+            <Link href="/blog">
             <a
-              href="#"
+
               className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-blue-600 bg-white hover:bg-gray-50 md:py-4 md:text-lg md:px-10"
             >
               Blog
             </a>
+            </Link>
           </div>
         </div>
       </div>
