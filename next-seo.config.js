89ba11f7e6ba5b1058d4eb9cffba1d71d89aa444
Felix Vemmer <felix.vemmer@gmail.com>
2021-12-04 10:33:47 +0100

    feat: adds SEO

---
+++ next-seo.config.js
@@ -0,0 +1,13 @@
+export default {
+    openGraph: {
+        type: 'website',
+        locale: 'en_US',
+        url: 'https://www.url.ie/',
+        site_name: 'Cryptoneur',
+    },
+    twitter: {
+        handle: '@FelixVemmer',
+        site: '@FelixVemmer',
+        cardType: 'summary_large_image',
+    },
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,18 @@
 import 'tailwindcss/tailwind.css';
+import { DefaultSeo } from 'next-seo';
+import { Seo } from '../next-seo.config';
 
 export default function MyApp({ Component, pageProps }) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout || ((page) => page)
 
   return getLayout(
-    <div className="bg-gray-50">
-      <Component {...pageProps} />
-    </div>
+    <>
+      <DefaultSeo {...Seo}/>
+      <div className="bg-gray-50">
+        <Component {...pageProps} />
+      </div>
+    </>
 
   )
 }
