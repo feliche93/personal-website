23579115964f500a44a0faf04aadf032922ae549
Felix Vemmer <felix.vemmer@gmail.com>
2021-12-05 22:58:59 +0100

    feat: fixes white background

--- components/layout/WebsiteLayout.js
+++ components/layout/WebsiteLayout.js
@@ -3,10 +3,10 @@ import Footer from './Footer'
 
 export default function WebsiteLayout(props) {
   return (
-    <>
+    <div className="bg-gray-50">
       <WebsiteNavbar></WebsiteNavbar>
       {props.children}
       <Footer></Footer>
-    </>
+    </div>
   )
 }
