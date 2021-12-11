1221d3ed103b1840d08f299a8a2fa54a01057525
Felix Vemmer <felix.vemmer@gmail.com>
2021-12-11 11:30:45 +0100

    chore: removes line

--- pages/portfolio/index.js
+++ pages/portfolio/index.js
@@ -2,7 +2,6 @@ import Layout from '../../components/layout/Layout'
 import WebsiteLayout from '../../components/layout/WebsiteLayout'
 import { NextSeo } from 'next-seo';
 import GridList from '../../components/portfolio/GridList';
-
 export default function index() {
     return (
         <>
