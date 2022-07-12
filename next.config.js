10548bf7373b65d89e0d826548ea41ba19194872
Felix Vemmer <felix.vemmer@gmail.com>
2022-07-13 01:07:38 +0200

    feat: plausible and new api key

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
-module.exports = {
-    images: {
-      domains: ['images.unsplash.com', 's3.us-west-2.amazonaws.com', 's3-us-west-2.amazonaws.com', 'pbs.twimg.com'],
-    },
-    i18n: {
-      locales: ["en"],
-      defaultLocale: "en",
-    },
-    swcMinify: false,
-  }
+const { withPlausibleProxy } = require('next-plausible')
+
+module.exports = withPlausibleProxy()({
+  images: {
+    domains: ['images.unsplash.com', 's3.us-west-2.amazonaws.com', 's3-us-west-2.amazonaws.com', 'pbs.twimg.com'],
+  },
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+  swcMinify: false,
+});
